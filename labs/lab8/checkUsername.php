9a29e84e84d9b3db3a3e8a8692f8cc0fcf0c7e6c
Alyssia Goodwin <algoodwin@csumb.edu>
2017-11-27 00:18:21 +0000

    Lab 8

---
+++ labs/lab8/checkUsername.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'dbConnection.php';
+$conn = getDatabaseConnection();
+
+
+$sql = "SELECT *
+        FROM tc_user
+        WHERE username = :username";
+
+$namedParameters = array();
+$namedParameters[':username'] = $_GET['$username'];
+
+        
+$stmt = $conn->prepare($sql);
+$stmt->execute($namedParameters);
+$record = $stmt->fetch(PDO::FETCH_ASSOC);//expecting only one record
+
+
+echo json_encode($record);
+
+?>
